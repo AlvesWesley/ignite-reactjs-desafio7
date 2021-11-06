816b00385c4267833767c299e879bde45598962d
Wesley Alves <wesley_alv@outlook.com>
2021-11-06 09:45:08 -0300

    concluindo desafio

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import { memo } from "react";
+
+interface HeaderProps {
+  title: string;
+}
+
+function HeaderComponent({ title }: HeaderProps) {
+  return (
+    <header>
+      <span className="category">
+        Categoria:<span> {title}</span>
+      </span>
+    </header>
+  );
+}
+
+export const Header = memo(HeaderComponent, (prevProps, nextProps) => {
+  return Object.is(prevProps, nextProps);
+});
